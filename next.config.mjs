0aff63cb2d34ac2eea4ccad52f051d6445b62ac0
Rana Essam <Ranaessam371@gmail.com>
2024-08-01 18:41:38 +0300

    Pure css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Import the required package
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+// Enable the analyzer based on the environment variable
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+// Define your Next.js configuration
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+};
+
+// Export the configuration wrapped with bundleAnalyzer
+export default bundleAnalyzer(nextConfig);
